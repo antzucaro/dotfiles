3261852bf8ea580c1c8ab0e7d9607b7d1d34784f
Ant Zucaro <azucaro@gmail.com>
2012-11-15 22:57:03 -0500

    Autostart Dropbox, ensure autostart happens only once.

--- rc.lua
+++ rc.lua
@@ -396,3 +396,20 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+
+-- {{{ Autostart
+-- run_once prevents auto-started applications from respawning
+function run_once(cmd)
+  findme = cmd
+  firstspace = cmd:find(" ")
+  if firstspace then
+    findme = cmd:sub(0, firstspace-1)
+  end
+  awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
+end
+
+-- and now the actual autostart programs
+run_once("~/.dropbox-dist/dropboxd")
+run_once("xfce4-power-manager")
+-- }}}
