4c714eb4c8d6f08a3bec99952bb382611b202f4b
Ant Zucaro <azucaro@gmail.com>
2012-11-18 08:30:08 -0500

    Use xfce4-terminal instead of gnome-terminal.

--- rc.lua
+++ rc.lua
@@ -34,7 +34,7 @@ end
 beautiful.init("/home/ant/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "gnome-terminal"
+terminal = "xfce4-terminal"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
